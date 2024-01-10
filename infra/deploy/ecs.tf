389f0da13a6575e570d782baab6fefdebbf6f459
Mark Winterbottom <londonappdev@duck.com>
2024-01-10 15:24:33 +0000

    added ecs container

---
+++ infra/deploy/ecs.tf
@@ -0,0 +1,103 @@
+##
+# ECS Cluster for running app on Fargate.
+##
+
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs."
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
+resource "aws_iam_role" "app_task" {
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-role-policy"
+  path        = "/"
+  description = "Policy to allow System Manager to execute in container"
+  policy      = file("./templates/ecs/task-ssm-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
+  role       = aws_iam_role.app_task.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
+}
+
+resource "aws_cloudwatch_log_group" "ecs_task_logs" {
+  name = "${local.prefix}-api"
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
+}
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "${local.prefix}-api"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.task_execution_role.arn
+  task_role_arn            = aws_iam_role.app_task.arn
+
+  container_definitions = jsonencode(
+    [
+      {
+        name              = "proxy"
+        image             = var.ecr_proxy_image
+        essential         = true
+        memoryReservation = 256
+        user              = "nginx"
+        portMappings = [
+          {
+            containerPort = 8000
+            hostPort      = 8000
+          }
+        ]
+        environment = [
+          {
+            name  = "APP_HOST"
+            value = "127.0.0.1"
+          }
+        ]
+        mountPoints = [
+          {
+            readOnly      = true
+            containerPath = "/vol/static"
+            sourceVolume  = "static"
+          }
+        ],
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
+            awslogs-region        = data.aws_region.current.name
+            awslogs-stream-prefix = "proxy"
+          }
+        }
+      }
+    ]
+  )
+
+  volume {
+    name = "static"
+  }
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
