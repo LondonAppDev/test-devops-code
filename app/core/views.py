3ee870050f82fd31ff9e4f543341e65c56ee7592
Mark Winterbottom <londonappdev@duck.com>
2024-01-14 16:45:59 +0000

    added print of meta

--- app/core/views.py
+++ app/core/views.py
@@ -8,4 +8,6 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def health_check(request):
     """Returns successful response."""
+    print("----- HEALTH CHECK ------")
+    print(request.META)
     return Response({'healthy': True})
